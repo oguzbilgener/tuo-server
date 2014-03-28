10f6cf6d32a5410e3f79824fa3d4f42ae27bd743
Oguz Bilgener <oguzbilgener@gmail.com>
2014-03-28 20:00:06 +0200

    do not use scala templates

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -3,12 +3,10 @@ package controllers;
 import play.*;
 import play.mvc.*;
 
-import views.html.*;
-
 public class Application extends Controller {
 
     public static Result index() {
-        return ok(index.render("Your new application is ready."));
+        return ok("Your new application is ready.");
     }
 
 }
