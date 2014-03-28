ec16efc6fbe3e8a49bdbf7a375ba468640613452
Oguz Bilgener <oguzbilgener@gmail.com>
2014-03-28 21:02:04 +0200

    Test importing from tuo-core

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,11 +1,17 @@
 package controllers;
 
+import co.uberdev.ultimateorganizer.core.CoreTask;
 import play.*;
 import play.mvc.*;
 
-public class Application extends Controller {
+public class Application extends Controller
+{
 
-    public static Result index() {
+    public static Result index()
+    {
+        // Test importing from tuo-core
+        CoreTask task = new CoreTask();
+
         return ok("Hello world.");
     }
 
