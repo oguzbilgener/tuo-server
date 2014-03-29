076ac554fd21f3f7369e0879ebcd5d45084c707f
Oguz Bilgener <oguzbilgener@gmail.com>
2014-03-29 12:28:58 +0200

    add dummy Task class

---
+++ app/co/uberdev/ultimateorganizer/server/models/Task.java
@@ -0,0 +1,36 @@
+package co.uberdev.ultimateorganizer.server.models;
+
+import co.uberdev.ultimateorganizer.core.CoreStorable;
+import co.uberdev.ultimateorganizer.core.CoreTask;
+
+/**
+ * Created by oguzbilgener on 29/03/14.
+ */
+public class Task extends CoreTask implements CoreStorable
+{
+    // Dummy Task class
+    public Task()
+    {
+        super();
+    }
+
+    @Override
+    public String getTableName() {
+        return null;
+    }
+
+    @Override
+    public boolean insert() {
+        return false;
+    }
+
+    @Override
+    public boolean update() {
+        return false;
+    }
+
+    @Override
+    public boolean remove() {
+        return false;
+    }
+}
