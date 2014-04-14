e4f083ca6dfa6621dd62c8b3ab8afc6d099959c5
Oguz Bilgener <oguzbilgener@gmail.com>
2014-04-14 12:28:29 +0300

    add sql setup for table users

---
+++ conf/sql/users.sql
@@ -0,0 +1,21 @@
+create table users (
+    id serial primary key,
+    email_address varchar(255), not null unique,
+    password_hashed varchar(255), not null,
+    first_name varchar(255),
+    last_name varchar(255),
+    public_key varchar(255) not null unique,
+    secret_token varchar(255), not null unique,
+    reset_key varchar(255),
+    reset_due int,
+    state smallint,
+    devices json,
+    school_name varchar(255),
+    department_name varchar(255),
+    created int,
+    birthday int
+);
+
+GRANT USAGE, SELECT ON SEQUENCE users_id_seq TO tuo_server_u;
+
+create index auth_index on users using public_token, secret_key;
