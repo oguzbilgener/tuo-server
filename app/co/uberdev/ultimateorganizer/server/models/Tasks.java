e81a7453934fd42518095c14c66658fbb20ab535
Ata Ali Kilicli <ataalikilicli@gmail.com>
2014-05-07 10:35:34 +0300

    loadfromDB

--- app/co/uberdev/ultimateorganizer/server/models/Tasks.java
+++ app/co/uberdev/ultimateorganizer/server/models/Tasks.java
@@ -62,21 +62,6 @@ public class Tasks extends CoreTasks implements CoreSelectable
                 task.setTaskOwnerNameCombined(set.getString(CoreDataRules.columns.tasks.taskOwnerNameCombined));
 
 
-                user.setId(set.getInt(CoreDataRules.columns.users.id));
-                user.setEmailAddress(set.getString(CoreDataRules.columns.users.emailAddress));
-                user.setPasswordHashed(set.getString(CoreDataRules.columns.users.passwordHashed));
-                user.setFirstName(set.getString(CoreDataRules.columns.users.firstName));
-                user.setLastName(set.getString(CoreDataRules.columns.users.lastName));
-                user.setPublicKey(set.getString(CoreDataRules.columns.users.publicKey));
-                user.setSecretToken(set.getString(CoreDataRules.columns.users.secretToken));
-                user.setState(set.getInt(CoreDataRules.columns.users.state));
-                user.setResetKey(set.getString(CoreDataRules.columns.users.resetKey));
-                user.setResetDue(set.getInt(CoreDataRules.columns.users.resetDue));
-                user.setCreated(set.getInt(CoreDataRules.columns.users.created));
-                user.setBirthday(set.getInt(CoreDataRules.columns.users.birthday));
-                user.setSchoolName(set.getString(CoreDataRules.columns.users.schoolName));
-                user.setDepartmentName(set.getString(CoreDataRules.columns.users.departmentName));
-
                 // TODO: set devices
 
                 add(task);
